f5ac5bedc86c7f4404856f1d879004886979f965
AjayiMike <ajayimikeofficial@gmail.com>
2022-11-30 17:21:19 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <GoogleOAuthProvider clientId="585509326929-74q3skkccqkmbtr5i64hubivlgsnq1su.apps.googleusercontent.com">
+            <Component {...pageProps} />
+        </GoogleOAuthProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
